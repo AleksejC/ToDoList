e3f5a5f879a15c5c6f4e8406561bb824947e21d7
AleksejC <dezmant949@gmail.com>
2023-10-07 15:15:56 +0200

    Task 2 commit

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import {useState} from "react";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import './ToDoList.css';
 
 export const ToDoList = () => {
+    const [openWindow, setOpenWindow] = useState(false);
+
+    const addNewTaskButton = () => {
+        if (openWindow) {
+            setOpenWindow(false);
+        } else {
+            setOpenWindow(true);
+        }
+    }
     return (
         <div className='container'>
             <h1 className='mainHeader'>Super TO-DO List</h1>
@@ -26,7 +40,46 @@ export const ToDoList = () => {
                 </div>
             </div>
 
-            <button className='addButton'>+</button>
+            <button className='addButton' onClick={addNewTaskButton}>+</button>
+
+            <Modal
+                open={openWindow}
+                onClose={addNewTaskButton}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className='newTaskBox'>
+                    <Typography id='modal-modal-title' className='title' variant='h4'>
+                        Adding new task
+                    </Typography>
+                    <Typography id='modal-modal-description' className='content'>
+                        <div className='contentDiv'>
+                            <p>New task name</p>
+                            <TextField
+                                className='newTaskNameInput'
+                                id="standard-helperText"
+                                defaultValue="Task 1"
+                                helperText="0/100 characters"
+                                variant="standard"
+                                size='medium'
+                            />
+                            <p>Description</p>
+                            <TextField
+                                className='descriptionInput'
+                                id="outlined-multiline-static"
+                                multiline
+                                rows={9}
+                                helperText='0/1000 characters'
+                            />
+                        </div>
+                            <hr/>
+                            <div className='modalButtonsContainer'>
+                                <button className='modalButtons'>Cancel</button>
+                                <button className='modalButtons'>Save</button>
+                            </div>
+                    </Typography>
+                </Box>
+            </Modal>
         </div>
     );
 }
