736ab8c5fc7f64c429716050a99526fd8b188fa2
AleksejC <dezmant949@gmail.com>
2023-10-08 01:34:45 +0200

    Task 2 commit

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,71 +1,81 @@
 import React from 'react';
+import {useCallback} from "react";
 import {useState} from "react";
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
 import './ToDoList.css';
 
 export const ToDoList = () => {
-    const [openWindow, setOpenWindow] = useState(false);
+    const [isAddTaskModal, setIsAddTaskModal] = useState(false);
+    const [isConfirmModal, setIsConfirmModal] = useState(false);
+    const [toDoTasks, setToDoTasks] = useState([]);
+    const [inProgressTasks, setInProgressTasks] = useState([]);
+    const [doneTasks, setDoneTasks] = useState([]);
 
-    const addNewTaskButton = () => {
-        if (openWindow) {
-            setOpenWindow(false);
-        } else {
-            setOpenWindow(true);
-        }
+    const toggleAddNewTaskModal = useCallback(() => setIsAddTaskModal(!isAddTaskModal), [isAddTaskModal])
+    const toggleCancelConfirmModal = useCallback(() => setIsConfirmModal(!isConfirmModal), [isConfirmModal])
+    const closeAllModals = () => {
+        toggleAddNewTaskModal();
+        toggleCancelConfirmModal();
     }
+
+    const onSaveButtonClick = () => {
+
+    }
+
     return (
         <div className='container'>
-            <h1 className='mainHeader'>Super TO-DO List</h1>
+            <h1 className='main-header'>Super TO-DO List</h1>
 
-            <div className ='columnsContainer'>
-                <div className='headerAndColumn'>
-                    <h1 className='columnHeader'>TO DO</h1>
+            <div className ='columns-container'>
+                <div className='header-and-column'>
+                    <h1 className='column-header'>TO DO</h1>
                     <div className='column'>
                     </div>
                 </div>
 
-                <div className='headerAndColumn'>
-                    <h1 className='columnHeader'>IN PROGRESS</h1>
+                <div className='header-and-column'>
+                    <h1 className='column-header'>IN PROGRESS</h1>
                     <div className='column'>
                     </div>
                 </div>
 
-                <div className='headerAndColumn'>
-                    <h1 className='columnHeader'>DONE</h1>
+                <div className='header-and-column'>
+                    <h1 className='column-header'>DONE</h1>
                     <div className='column'>
                     </div>
                 </div>
             </div>
 
-            <button className='addButton' onClick={addNewTaskButton}>+</button>
+            <button className='add-new-task-button' onClick={toggleAddNewTaskModal}>+</button>
 
             <Modal
-                open={openWindow}
-                onClose={addNewTaskButton}
+                open={isAddTaskModal}
+                onClose={toggleCancelConfirmModal}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box className='newTaskBox'>
+                <Box className='new-task-box'>
                     <Typography id='modal-modal-title' className='title' variant='h4'>
                         Adding new task
                     </Typography>
                     <Typography id='modal-modal-description' className='content'>
-                        <div className='contentDiv'>
-                            <p>New task name</p>
+                        <div className='content-div'>
+                            <label htmlFor='standard-helper-text'>New task name</label>
                             <TextField
-                                className='newTaskNameInput'
-                                id="standard-helperText"
+                                className='new-task-name-input'
+                                id="standard-helper-text"
                                 defaultValue="Task 1"
                                 helperText="0/100 characters"
                                 variant="standard"
                                 size='medium'
                             />
-                            <p>Description</p>
+                            <label htmlFor='outlined-multiline-static'>Description</label>
                             <TextField
-                                className='descriptionInput'
+                                className='description-input'
                                 id="outlined-multiline-static"
                                 multiline
                                 rows={9}
@@ -73,11 +83,39 @@ export const ToDoList = () => {
                             />
                         </div>
                             <hr/>
-                            <div className='modalButtonsContainer'>
-                                <button className='modalButtons'>Cancel</button>
-                                <button className='modalButtons'>Save</button>
+                            <div className='modal-buttons-container'>
+                                <button className='modal-button' onClick={toggleCancelConfirmModal}>Cancel</button>
+                                <button className='modal-button'>Save</button>
                             </div>
                     </Typography>
+                </Box>
+            </Modal>
+            <Modal
+                open={isConfirmModal}
+                onClose={toggleCancelConfirmModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box className='confirm-box'>
+                    <Typography id="modal-modal-description">
+                        <WarningRoundedIcon
+                            color='warning'
+                            sx={{ fontSize: 60 }}
+                            className='warning-icon'
+                        />
+                        <div className='confirm-div'>
+                            <div className='confirm-title-div'>
+                                WARNING
+                            </div>
+                            <div className='confirm-content-div'>
+                                The current task will not be saved. Cancel anyway?
+                            </div>
+                        </div>
+                    </Typography>
+                    <div className='confirm-buttons'>
+                        <button className='confirm-button' onClick={toggleCancelConfirmModal}>No</button>
+                        <button className='confirm-button' onClick={closeAllModals}>Yes</button>
+                    </div>
                 </Box>
             </Modal>
         </div>
